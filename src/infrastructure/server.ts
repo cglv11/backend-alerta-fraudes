5b3fbb018159a150123519bf10ef63085fc19f76
Camilo <cglv11@gmail.com>
2026-01-12 17:08:31 -0500

    feat: server infraestructure

--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -2,9 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import { AppDataSource } from './database/data-source'
 import { PostgresTransactionRepository } from './repositories/PostgresTransactionRepository'
+import { PostgresFraudDetectionRepository } from './repositories/PostgresFraudDetectionRepository'
 import { CreateTransaction } from '../application/use-cases/CreateTransaction'
+import { GetUserRiskProfile } from '../application/use-cases/GetUserRiskProfile'
 import { TransactionController } from '../interfaces/controllers/TransactionController'
+import { FraudDetectionController } from '../interfaces/controllers/FraudDetectionController'
 import { transactionRoutes } from '../interfaces/routes/transaction.routes'
+import { fraudDetectionRoutes } from '../interfaces/routes/fraud-detection.routes'
 
 async function bootstrap() {
   await AppDataSource.initialize()
@@ -13,20 +17,25 @@ async function bootstrap() {
   app.use(cors())
   app.use(express.json())
 
-  // Infrastructure
-  const transactionRepository =
-    new PostgresTransactionRepository()
+  // Infrastructure - Repositories
+  const transactionRepository = new PostgresTransactionRepository()
+  const fraudDetectionRepository = new PostgresFraudDetectionRepository(
+    transactionRepository
+  )
 
-  // Use case
-  const createTransaction =
-    new CreateTransaction(transactionRepository)
+  // Use cases
+  const createTransaction = new CreateTransaction(transactionRepository)
+  const getUserRiskProfile = new GetUserRiskProfile(fraudDetectionRepository)
 
-  // Controller
-  const transactionController =
-    new TransactionController(createTransaction)
+  // Controllers
+  const transactionController = new TransactionController(createTransaction)
+  const fraudDetectionController = new FraudDetectionController(
+    getUserRiskProfile
+  )
 
   // Routes
   app.use('/api', transactionRoutes(transactionController))
+  app.use('/api', fraudDetectionRoutes(fraudDetectionController))
 
   const PORT = process.env.PORT || 3000
   app.listen(PORT, () => {
