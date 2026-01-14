1fe8bd1e4da479f6f5dcd3ff4b323eb6c0d43f84
Camilo <cglv11@gmail.com>
2026-01-13 20:45:05 -0500

    feat: transaction controller

--- src/interfaces/controllers/TransactionController.ts
+++ src/interfaces/controllers/TransactionController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express'
 import { CreateTransaction } from '../../application/use-cases/CreateTransaction'
+import { GetUsers } from '../../application/use-cases/GetUsers'
 
 export class TransactionController {
   constructor(
-    private readonly createTransaction: CreateTransaction
+    private readonly createTransaction: CreateTransaction,
+    private readonly getUsersUseCase: GetUsers // Renamed to avoid conflict
   ) {}
 
   async create(req: Request, res: Response) {
@@ -20,6 +22,25 @@ export class TransactionController {
         amount: transaction.amount,
         country: transaction.country,
         createdAt: transaction.createdAt,
+      })
+    } catch (error) {
+      return res.status(400).json({
+        error: (error as Error).message,
+      })
+    }
+  }
+
+  // Get all users
+  async getUsers(req: Request, res: Response) {
+    try {
+      const users = await this.getUsersUseCase.execute() // Use renamed property
+
+      return res.status(200).json({
+        users: users.map((user) => ({
+          userId: user.userId,
+          transactionCount: user.transactionCount,
+          lastTransactionDate: user.lastTransactionDate,
+        })),
       })
     } catch (error) {
       return res.status(400).json({
