4e2be143049271db6635aa89faa260613ca624fe
Camilo <cglv11@gmail.com>
2026-01-13 20:45:16 -0500

    feat: transaction routes

--- src/interfaces/routes/transaction.routes.ts
+++ src/interfaces/routes/transaction.routes.ts
@@ -1,14 +1,13 @@
 import { Router } from 'express'
 import { TransactionController } from '../controllers/TransactionController'
 
-export function transactionRoutes(
-  controller: TransactionController
-) {
+export function transactionRoutes(controller: TransactionController) {
   const router = Router()
 
-  router.post('/transactions', (req, res) =>
-    controller.create(req, res)
-  )
+  router.post('/transactions', (req, res) => controller.create(req, res))
+
+  // New route
+  router.get('/users', (req, res) => controller.getUsers(req, res))
 
   return router
 }
